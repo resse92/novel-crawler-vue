d5c385191406e79488430d651d1b79b20119cdd7
zhuruliang <zrl920825@163.com>
2017-09-04 08:47:26 +0800

    fix filename

---
+++ src/components/homepage/CategoryBooks.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="categories">
+    <ul>
+      <li v-for="(books, category) in categories">
+        <category-book :books="books" :category="category"></category-book>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import CategoryBook from './CategoryBook'
+export default {
+  name: 'CategoryBooks',
+  props: ['categories'],
+  components: {
+    CategoryBook
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+blueColor = #93C2C0
+*
+  padding: 0px
+  margin: 0px
+  float: left
+  overflow: hidden
+div#categories
+  padding: 3px
+  width: 980px
+  border-style: solid
+  border-color blueColor
+  border-width: 2px
+  margin: 5px 0 5px 0
+  ul
+    list-style: none outside none
+    width: 100%
+    li
+      width: 33.25%
+      line-height: 26px
+    li:nth-child(1), li:nth-child(2)
+      border-right-color: blueColor
+      border-right-style: dotted
+      border-right-width: 1px
+
+</style>
